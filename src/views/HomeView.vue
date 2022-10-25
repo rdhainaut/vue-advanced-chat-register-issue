82edea10e863c601535bbf75652fc391362ae331
Raphael Dhainaut <rdhainaut@actiris.be>
2022-10-25 20:45:46 +0200

    Add vue advanced chat component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <VueChat />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import VueChat from "@/components/VueChat.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
+    VueChat,
   },
 })
 export default class HomeView extends Vue {}
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => ({
+        ...options,
+        compilerOptions: {
+          // treat any tag that starts with ion- as custom elements
+          isCustomElement: tagName => tagName === 'vue-advanced-chat'
+        }
+      }))
+  }
+}
